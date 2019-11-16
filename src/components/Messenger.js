7c8ea806c3f850684a8e1cb84e658b87247d845f
Макаров Максим <MisterMaks97@mail.ru>
2019-11-16 21:27:43 +0300

    ДЗ-6

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -140,7 +140,7 @@ export class Messenger extends Component {
 			<Router>
 				<React.Fragment>
 					<Switch>
-						<Route path="//">
+						<Route path="/">
 							<DialogForm
 								/* styleDialogForm={this.state.styleDialog} */
 								/* 
